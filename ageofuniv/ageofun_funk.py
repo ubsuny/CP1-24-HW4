b9cc194cf0315b12fb873eab070f686f412b5242
zbpetersbuf <zbpeters@buffalo.edu>
2024-10-30 22:17:38 +0000

    madefunction mabye

--- ageofuniv/ageofun_funk.py
+++ ageofuniv/ageofun_funk.py
@@ -1,3 +1,9 @@
 """
 the code stuff
+1 megaparsec (3.09*10**19 km)
 """
+
+def age (H_CONS):
+    """So for this it takes Hubbles cons and outputs the age"""
+    C=3.09*(10**19)
+    return C*H_CONS**(-1)
