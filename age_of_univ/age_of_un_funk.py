14289b1f65a9db9fa96fc2ce24e0af92e73b65b1
zbpetersbuf <zbpeters@buffalo.edu>
2024-10-30 22:28:35 +0000

    ready to be pull requested but not accepted

--- age_of_univ/age_of_un_funk.py
+++ age_of_univ/age_of_un_funk.py
@@ -1,9 +1,11 @@
 """
 the code stuff
 1 megaparsec (3.09*10**19 km)
+this is for if i have the constant already if not ill need to revies it, since
+the hubbles constant is velocity moving away from earth over distance from earth it shouldnt be hard
 """
 
-def age (H_CONS):
+def age (hub_cons):
     """So for this it takes Hubbles cons and outputs the age"""
-    C=3.09*(10**19)
-    return C*H_CONS**(-1)
+    MEGPAR_TO_KM =3.09*(10**19)   # pylint: disable=C0103
+    return MEGPAR_TO_KM*(hub_cons**(-1))
