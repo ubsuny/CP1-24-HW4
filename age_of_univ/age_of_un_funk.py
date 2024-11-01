ce89a62382cf12f750d721df71cea5a658c1d7a1
zbpetersbuf <zbpeters@buffalo.edu>
2024-11-01 15:47:36 +0000

    fixed flake8 and pytest

--- age_of_univ/age_of_un_funk.py
+++ age_of_univ/age_of_un_funk.py
@@ -5,7 +5,8 @@ to revies it, since the hubbles constant is velocity moving
 away from earth over distance from earth it shouldnt be hard
 """
 
-def age (hub_cons):
+
+def age(hub_cons):
     """So for this it takes Hubbles cons and outputs the age"""
-    MEGPAR_TO_KM=3.09*(10**19) # pylint: disable=C0103
-    return MEGPAR_TO_KM*(hub_cons**(-1))
+    MEGPAR_TO_KM = 3.09 * (10 ** 19)  # pylint: disable=C0103
+    return MEGPAR_TO_KM * (hub_cons ** (-1))
